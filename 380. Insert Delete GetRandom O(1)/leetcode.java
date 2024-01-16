ff762271a52074f91312bbe7713141c2b02ba0c9
Nehal Ingole <nehal712521@gmail.com>
2024-01-16 19:13:56 +0530

    Medium level question in java

---
+++ 380. Insert Delete GetRandom O(1)/leetcode.java
@@ -0,0 +1,34 @@
+class RandomizedSet {
+    private Map<Integer, Integer> map;
+    private Random random;
+    private List<Integer> elements;
+
+    public RandomizedSet() {
+        map = new HashMap<>();
+        random = new Random();
+        elements = new ArrayList<>();
+    }
+    public boolean insert(int val) {
+        if (!map.containsKey(val)) {
+            elements.add(val);
+            map.put(val, elements.size() - 1);
+            return true;
+        }
+        return false;
+    }
+    public boolean remove(int val) {
+        int lastIndex = map.getOrDefault(val, -1);
+        if (lastIndex == -1)
+            return false;
+        Collections.swap(elements, lastIndex, elements.size() - 1);
+        int otherSwappedValue = elements.get(lastIndex);
+        map.put(otherSwappedValue, lastIndex);
+        elements.remove(elements.size() - 1);
+        map.remove(val);
+        return true;
+    }
+    public int getRandom() {
+        int randomIndex = random.nextInt(elements.size());
+        return elements.get(randomIndex);
+    }
+}
