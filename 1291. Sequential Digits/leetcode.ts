7ba43ee81e5ce1cc6df42dd46c69145c2451c932
Nehal Ingole <73941735+Ingole712521@users.noreply.github.com>
2024-02-02 21:29:56 +0530

    Create leetcode.ts
    Medium level

---
+++ 1291. Sequential Digits/leetcode.ts
@@ -0,0 +1,14 @@
+function sequentialDigits(low: number, high: number): number[] {
+    let result: number[] = [];
+    for (let startDigit = 1; startDigit < 9; ++startDigit) {
+        let num = startDigit;
+        for (let nextDigit = startDigit + 1; nextDigit < 10; ++nextDigit) {
+            num = num * 10 + nextDigit;
+            if (num >= low && num <= high) {
+                result.push(num);
+            }
+        }
+    }
+    result.sort((a, b) => a - b);
+    return result;
+}
